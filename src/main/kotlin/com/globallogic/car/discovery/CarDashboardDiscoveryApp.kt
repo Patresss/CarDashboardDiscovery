3dee2e8da285d220458117fc275cd60f20194a4e
Patresss <patresss@wp.pl>
2020-01-21 21:20:19 +0100

    Add Spring Admin

--- src/main/kotlin/com/globallogic/car/discovery/CarDashboardDiscoveryApp.kt
+++ src/main/kotlin/com/globallogic/car/discovery/CarDashboardDiscoveryApp.kt
@@ -1,5 +1,6 @@
 package com.globallogic.car.discovery
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 class CarDashboardDiscoveryApp
 
 fun main() {
